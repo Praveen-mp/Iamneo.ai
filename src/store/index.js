5c360457409defe6969a13b175c0b1c73ee4d4b2
praveen <pravinkabi6@gmail.com>
2023-07-13 13:42:34 +0530

    minorFix

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import columnsReducer from './slices/column.slice';
+import cardsReducer from './slices/card.slice';
+
+const store = configureStore({
+  reducer: {
+    columns: columnsReducer,
+    cards: cardsReducer
+  }
+})
+
+export const RootState = store.getState();
+export const AppDispatch = store.dispatch;
+
+export default store
